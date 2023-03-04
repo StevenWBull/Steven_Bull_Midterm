55418f9277cd89624fdb2a0ccfacf4fef51bbfe4
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-03-04 16:21:42 -0600

    add in options check into Router.php

--- Router.php
+++ Router.php
@@ -24,6 +24,12 @@ class Router {
         $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
         $method = $_SERVER['REQUEST_METHOD'];
 
+        if ($method === 'OPTIONS') {
+            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+            header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+            exit();
+        }
+
         $route_found = false;
         
         if(isset($this->routes[$method])) {
